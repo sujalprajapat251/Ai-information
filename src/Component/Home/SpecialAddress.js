f03b0413eb6c4f7973d77f40c10acb4a2bcc644d
sujalprajapat251 <sujal.kalathiyainfotech@gmail.com>
2025-08-13 17:59:32 +0530

    sujal13-08

---
+++ src/Component/Home/SpecialAddress.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Stack from "../../reactBeats/Stack";
+import person1 from '../../Asset/Home/person1.webp'
+import person2 from '../../Asset/Home/person2.webp'
+import person3 from '../../Asset/Home/person3.png'
+import person4 from '../../Asset/Home/person4.webp'
+import person5 from '../../Asset/Home/person5.webp'
+const images = [
+    { id: 1, img: person1 },
+    { id: 2, img: person2 },
+    { id: 3, img: person3 },
+    { id: 4, img: person4 },
+    { id: 5, img: person5 }
+];
+const SpecialAddress = () => {
+  return (
+
+    <section className="bg-black xl:py-10 py-6">
+    <div className="lg:flex flex-wrap justify-center items-center container">
+        <div className="px-8 lg:w-1/2">
+            <h2 className="2xl:text-5xl md:text-3xl  text-2xl font-semibold text-white md:mb-5 mb-3 2xl:leading-[60px]" data-aos="fade-up"><span className="text-themedark fond-extrabold">NVIDIA</span> at SIGGRAPH Research Special Address</h2>
+            <p className="text-[#f0f0f0]  sm:text-base text-xs md:mb-5 mb-3  " data-aos="fade-up">New <span className="text-themedark">NVIDIA</span> technologies are enabling instant world reconstruction, spatial reasoning, and large-scale simulation on NVIDIA DGX Cloud and NVIDIA RTX PRO Blackwell servers.</p>
+        </div>
+        <>
+            {/* Large screen */}
+            <div className="hidden sm:block" data-aos="fade-left">
+                <Stack
+                    cardDimensions={{ width: 400, height: 400 }}
+                    cardsData={images}
+                />
+            </div>
+
+            {/* Small screen */}
+            <div className="block sm:hidden" data-aos="fade-left">
+                <Stack
+                    cardDimensions={{ width: 250, height: 250 }}
+                    cardsData={images}
+                />
+            </div>
+        </>
+    </div>
+</section>
+  )
+}
+
+export default SpecialAddress
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import Introduction from '../Component/Home/Introduction'
+import AiandHowitsWork from '../Component/Home/AiandHowitsWork'
+import SpecialAddress from '../Component/Home/SpecialAddress'
+import team from '.././Asset/About/about_group.jpg'
+import Stories from '../Component/Home/Stories'
 
 const Home = () => {
   return (
     <div className=''>
         <Introduction/>
+
+        <AiandHowitsWork/>
+        <SpecialAddress/>
+        
+      <Stories/>
+      <section className='bg-black'>
+         <div className='pt-10'>
+              <div className='container mx-auto'>
+                 <div className='flex lg:flex-nowrap flex-wrap rounded-[30px] overflow-hidden 2xl:mx-10 mx-5'>
+                     <div className="lg:w-1/2 w-full">
+                         <img src={team} alt="" className='h-full object-cover'/>
+                     </div>
+                     <div className='lg:w-1/2 w-full bg-[#222222]'>
+                         <div className='sm:p-[65px] sx:p-[40px] p-[20px] text-white'>
+                            <p>Mentorship programs</p>
+                            <h3 className="text-white text-[22px] sm:text-[32px] md:text-[40px] lg:text-[40px] sx:leading-[46px] leading-[28px] mt-4">Get hands-on experience in AI <br /> research</h3>
+                            <p className='sm:text-[16px] text-[14px] mt-10 sm:pt-4'>We’re passionate about introducing people to the extraordinary world of AI. Our AI Mentorship (AIM) and EMEA programs let PhD candidates conduct open science research in collaboration with their university, academic advisor, and AI at Meta mentor participants as full-time AI at Meta employees.</p>
+                            <p className='sm:text-[16px] text-[14px] mt-5'>We currently have agreements in place with prestigious universities in the United States, France, Israel and the United Kingdom.</p>
+                            <p className='mt-10 sm:pt-4 sm:text-[18px] text-[16px] font-[600]'>For more about these programs, reach out to us at MetaAIM@meta.com</p>
+                         </div>
+                     </div>
+                 </div>
+              </div>
+         </div>
+      </section>
     </div>
   )
 }
